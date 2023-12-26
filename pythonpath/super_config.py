b237f874b15cac183a6ccd597830e92435339296
xj <xiaojun.zhang@nyonic.com>
2023-12-26 09:39:13 +0800

    fork and add config.py to PTYHONPATH

---
+++ pythonpath/super_config.py
@@ -0,0 +1,9 @@
+# Superset specific config
+ROW_LIMIT = 5000
+
+# Flask-WTF flag for CSRF
+WTF_CSRF_ENABLED = False
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
